5053f99c9ffaf9601032220ed13785a40f8dd81c
Lorenzo-Malatesta99 <lorenzo.ma932@gmail.com>
2024-11-21 16:09:44 +0100

    ADD milestone 1-2-3

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const postsRouter = require("./routers/router.js");
+const bodyParser = require("body-parser");
 
 app.use(express.static("public"));
 
 app.use(express.json());
+
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   console.log("Questa è la root!");
--- controllers/controller.js
+++ controllers/controller.js
@@ -15,11 +15,47 @@ function show(req, res) {
   res.json(findPost);
 }
 
-function store(req, res) {}
+function store(req, res) {
+  const { title, slug, content, image, tags } = req.body;
+  console.log("Dati in arrivo:", req.body);
+  const newPost = {
+    id: posts.length + 1,
+    title,
+    slug,
+    content,
+    image,
+    tags,
+  };
+  posts.push(newPost);
+  console.log("Nuovo post creato:", newPost);
+  res.status(201).json(newPost);
+}
 
-function update(req, res) {}
 
-function modify(req, res) {}
+function update(req, res) {
+  const id = parseInt(req.params.id);
+  const { title, content } = req.body;
+  const postIndex = posts.findIndex((post) => post.id === id);
+  if (postIndex === -1) {
+    return res.status(404).json({ error: "Post non trovato" });
+  }
+  posts[postIndex] = { id, title, content };
+  console.log("Post aggiornato:", posts[postIndex]);
+  res.json(posts[postIndex]);
+}
+
+function modify(req, res) {
+  const id = parseInt(req.params.id);
+  const { title, content } = req.body;
+  const post = posts.find((post) => post.id === id);
+  if (!post) {
+    return res.status(404).json({ error: "Post non trovato" });
+  }
+  if (title) post.title = title;
+  if (content) post.content = content;
+  console.log("Post modificato:", post);
+  res.json(post);
+}
 
 function destroy(req, res) {
   const id = parseInt(req.params.id);
